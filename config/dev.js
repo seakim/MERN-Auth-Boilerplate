6828c555b092e574e2098086ebf55367f00bcfad
sk <dev.seakim@gmail.com>
2018-10-12 14:16:45 -0400

    completed backend with google OAuth

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: "19624254470-s5lmivaaofe79ft1cj70elnfktb2bghk.apps.googleusercontent.com",
+    googleClientSecret: "E7sunQNgqUc0yXmrEkovSF60",
+    mongoURI: "mongodb://localhost/oAuth-dev",
+    cookieKey: "alsdkfjweufasje23iopurwjlasjfldasmA234jeklwDasdkljcxkmw239pulkjeA"
+}
